ad80a73b8cb638d88d7416a3b883c232b70f6934
ajuroshan <ajuaju0483@gmail.com>
2024-08-22 02:37:26 +0530

    admin page setup

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Application
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+	list_display = ['applicant','mess_no','accepted','created_at']
+	list_filter = ['applicant','hostel','mess_no','accepted','created_at']
+	search_fields = ['applicant','hostel','accepted','created_at']
+	list_per_page = 10
--- application/models.py
+++ application/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from mess.models import *
 
 # Create your models here.
+
+hostels = [
+	('Sagar', 'Sagar'),
+
+]
+
+class Application(models.Model):
+	applicant = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+	mess_no = models.IntegerField(default=100)
+	hostel = models.CharField(max_length=100, choices=hostels)
+	accepted = models.BooleanField(default=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	messcuts = models.ManyToManyField(Messcut, blank=True)
+
+	def __str__(self):
+		return str(self.applicant.username + ' - ' + self.created_at.strftime('%d-%m-%Y'))
+
+
