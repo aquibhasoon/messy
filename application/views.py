e44fe486f0b2af934f257eafbc231556e2dd04f5
aquibhasoon <aquibhas2793@gmail.com>
2024-08-24 15:50:08 +0530

    created a profile page

--- application/views.py
+++ application/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'application/home.html', {})
+
+def profile(request):
+	return render(request, 'application/profile.html', {})
